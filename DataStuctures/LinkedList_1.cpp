e1904aed8b09dc71cb564ea0cf9c21cf54e63f66
Vaibhav Rathod <vaibhavrr18@gmail.com>
2022-03-03 17:02:48 +0530

    1st

---
+++ DataStuctures/LinkedList_1.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+void insertAtHead(node* &head,int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+void insertAtTail(node* &head,int val){
+    
+    node* n=new node(val);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+
+    }
+    temp->next=n;
+}
+void deleteHead(node* &head){
+    node* todelete=head;
+    head=head->next;
+    delete todelete;
+}
+void deletion(node* &head,int val){
+    node* temp=head;
+    while(temp->next->data!=val){
+        temp=temp->next;
+    }
+    node* todelete=temp->next;
+    temp->next=temp->next->next;
+    delete todelete;
+}
+
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
